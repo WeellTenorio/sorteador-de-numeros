a054518f8fe68aac39f36d459387bd4368156740
Wellington Tenorio <wellington__tenorio@hotmail.com>
2025-06-25 23:31:59 -0400

    adicionar leitura por voz

--- app.js
+++ app.js
@@ -27,6 +27,15 @@ function sortear(){
     
     let resultado = document.getElementById('resultado');
     resultado.innerHTML =` <label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+    let texto = `Números sorteados: ${sorteados.join(', ')}`;
+    if ('speechSynthesis' in window) {
+        let utterance = new SpeechSynthesisUtterance(texto);
+        utterance.lang = 'pt-BR'; 
+        utterance.rate = 1.2; 
+        window.speechSynthesis.speak(utterance); 
+    } else {
+        console.log("Web Speech API não suportada neste navegador.");
+    }
     alterarStatusBotao();
 
 }
